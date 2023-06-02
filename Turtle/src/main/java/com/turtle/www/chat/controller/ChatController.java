9267e20072615e8718ba98c779f9128f0e6e4a34
mskim9097@gmail.com <mskim9097@gmail.com>
2023-06-02 15:18:53 +0900

    채팅방 작업중

--- Turtle/src/main/java/com/turtle/www/chat/controller/ChatController.java
+++ Turtle/src/main/java/com/turtle/www/chat/controller/ChatController.java
@@ -2,19 +2,29 @@ package com.turtle.www.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.turtle.www.chat.model.service.ChatService;
+import com.turtle.www.member.model.vo.Member;
 
 @RequestMapping("/chat")
 @Controller
 public class ChatController {
 	
-
+	@Autowired
+	private ChatService service;
 	
+	// 채팅방 목록 조회
 	@GetMapping("/chatRoomList")
-	public String chatRoomList() {
+	public String chatRoomList(@ModelAttribute("loginMember") Member loginMember ,Model model) {
+		
+		
+		
+		//워크스페이스 채팅방 리스트 조회		
+		//개인 채팅방용 프로젝트 멤버 조회
 		
 		return "chat/chatRoomList";
 	}
--- Turtle/src/main/java/com/turtle/www/project/model/vo/Project.java
+++ Turtle/src/main/java/com/turtle/www/project/model/vo/Project.java
@@ -1,5 +1,22 @@
 package com.turtle.www.project.model.vo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Project {
 
+	private int projectNo; // 프로젝트넘버
+	private String projectName; // 프로젝트 이름
+	private String projectEmoji; // 프로젝트 이미지
+	private String projectDescription; // 프로젝트 설명
+	private String inviteCode; // 프로젝트 초대코드
+	private String projectDeleteFG; // 프로젝트 삭제여부
+	private String regDate; // 프로젝트 생성일
+	
 }
