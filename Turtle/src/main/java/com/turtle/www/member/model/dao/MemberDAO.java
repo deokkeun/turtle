d1fa26b18280c3d0fb03415b8de0d8893981ebee
deokkeun <deokk91@gmail.com>
2023-07-09 22:42:03 +0900

    Fix: 네이버 이미지변경

--- Turtle/src/main/java/com/turtle/www/member/model/dao/MemberDAO.java
+++ Turtle/src/main/java/com/turtle/www/member/model/dao/MemberDAO.java
@@ -1,13 +1,11 @@
 package com.turtle.www.member.model.dao;
 
-import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.turtle.www.chat.model.vo.ChatRoom;
 import com.turtle.www.member.model.vo.Certification;
 import com.turtle.www.member.model.vo.Member;
 
